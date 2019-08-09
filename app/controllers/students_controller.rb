d2750a9d71d0f29e03dba4dcb8d94cec8605668d
Bry Cox <carl.cc@gmail.com>
2019-08-09 18:10:20 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,6 +18,12 @@ class StudentsController < ApplicationController
 	  redirect_to student_path(@student)
   end
 
+  def update
+	  @student = Student.find(params[:id])
+	  @student.update(student_params(:first_name, :last_name))
+	  redirect_to student_path(@sc)
+	end
+
   private
 
     def student_params(*args)
