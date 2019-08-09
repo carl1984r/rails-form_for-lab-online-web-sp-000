491594a3ab647a474e498fe467234c938bdfcf70
Bry Cox <carl.cc@gmail.com>
2019-08-09 17:08:53 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,21 @@
 class SchoolClassesController < ApplicationController
 
+
+  def new
+
+  end
+
+  def create
+	  @sc = SchoolClass.new(school_class_params(:title, :room_number))
+	  @sc.save
+	  redirect_to school_class_path(@sc)
+	end
+
+  private
+
+    def school_class_params(*args)
+      params.require(:school_class).permit(*args)
+    end
+
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,16 @@
 class StudentsController < ApplicationController
 
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+	  @student.save
+	  redirect_to student_path(@student)
+  end
+
+  private
+
+    def student_params(*args)
+      params.require(:student).permit(*args)
+    end
+
+
 end
